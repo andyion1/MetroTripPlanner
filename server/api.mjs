058b297dfb942f81a2a3b3bb3b3b7627f5f95516
Andy Ionita <andyionita06@gmail.com>
2025-10-01 14:12:24 -0400

    Add necessary imports for api.mjs

--- server/api.mjs
+++ server/api.mjs
@@ -1,4 +1,7 @@
 import express from 'express';
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = process.env.PORT || 3000;
